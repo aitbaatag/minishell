b25272497fddca34b784012977ba29fdf0f48402
Khalid Ait Baatag <kait-baa@c3r7p3.1337.ma>
2024-06-05 14:19:42 +0100

    fix pipe hanging

--- include/executer.h
+++ include/executer.h
@@ -12,7 +12,7 @@ int	run_cmd(t_tree *tree);
 int	run_subshell(t_tree *subshell);
 int	run_redir();
 
-int		get_status(t_tree *tree);
+int		ft_run_node(t_tree *tree);
 int		get_exit_status(void);
 void	set_exit_status(int status);
 void	executer(t_tree *tree);
